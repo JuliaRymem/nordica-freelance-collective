41b3e3946e40f293730870e6ca2e66755759266a
Emil Asikainen <emilasikainen@hotmail.com>
2025-05-14 11:17:13 +0200

    uppdaterade sanityClient och package-lock json

--- frontend/src/sanityClient.js
+++ frontend/src/sanityClient.js
@@ -1,8 +1,9 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "nordicFreelance",
+  projectId: "6g2l3qkq",
   dataset: "production",
   apiVersion: "2023-01-01",
   useCdn: true,
+  token: "sk0OFA4zatbU3jvFTPf1hb82h1uOJhOS2Y3EQ0PyciSg0B7zabVQz5p2nTYHAeXmAANaa0mmuaaRyx2By689ASl91tRTQlZIinM8kzhJegysUFbYxYhGXXH15UXiX0M0lCP91EVyMkxHOe4TvLFldUGsfjEyMhXmP2MJSFAsjlOP1n3VJ092,"
 });
