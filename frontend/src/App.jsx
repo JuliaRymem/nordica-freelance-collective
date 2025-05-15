92574ed022cb93f9387c68fc63a50f92967b0931
Viktorwahlqvist <wahlqvistviktor@hotmail.com>
2025-05-15 14:14:19 +0200

    local

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import SinglePageProject from "./features/products/pages/SinglePageProject";
 import FreelancerList from "./features/products/pages/userProfilePage";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1>Våra frilansare</h1>
       <FreelancerList />
-    </>
+
+      <SinglePageProject />
+    </QueryClientProvider>
   );
 }
 
--- frontend/src/sanityClient.js
+++ frontend/src/sanityClient.js
@@ -1,8 +1,10 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "nordicFreelance",
+  projectId: "6g2l3qkq",
   dataset: "production",
+  token:
+    "skjtTvBRbrR9jzEb7nfU4mAxtGyJSxOahvCPsjUsFzbA6hspjE6qugLbbZUaV26uHak0zQWRuFuo0SWYVTnPGwKRQfUqVD4knITdBNkS0zphrPQyg0mw9aMTykS1w9zi14FjsNIjfDfTd9U90CojzH73PPs7BVWxcSdR9afstvF3VQ6wYNfY",
   apiVersion: "2023-01-01",
   useCdn: true,
 });
