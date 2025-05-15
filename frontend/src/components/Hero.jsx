8663c1957296404b8a4094a97ca7df9253c5edd5
JuliaRymem <juliakrasmusson@hotmail.com>
2025-05-15 13:08:05 +0200

    Sektion trusted freelancer - test

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -11,18 +11,19 @@ function Hero() {
       </div>
 
       {/* Hero Text */}
-      <div className="text-center mb-9">
-        <h1 className="text-3xl md:text-5xl font-bold leading-snug text-white">
-          NORDIC <br /> FREELANCE COLLECTIVE
-        </h1>
-      </div>
+      <h1
+  className="text-2xl md:text-4xl font-bold leading-snug text-white text-center mb-6"
+  style={{ textShadow: "5px 3px 10px rgba(0,0,0,0.7)" }}
+>
+  NORDIC <br /> FREELANCE COLLECTIVE
+</h1>
 
       {/* Knapp */}
       <div className="flex justify-center space-x-4 mb-6">
-        <button className="px-6 py-3 rounded-full border border-black bg-white text-black font-semibold">
+        <button className="px-6 py-3 rounded-full border border-black bg-white hover:bg-amber-50 text-black font-semibold">
           LOG IN
         </button>
-        <button className="px-6 py-3 rounded-full border border-black bg-black text-white font-semibold">
+        <button className="px-6 py-3 rounded-full border border-black bg-black hover:bg-black-200 text-white font-semibold">
           JOIN US
         </button>
       </div>
@@ -39,6 +40,22 @@ function Hero() {
         <ArrowRight className="text-black w-5 h-5" />
       </div>
 
+
+      {/* Trusted Freelancers-sektion */}
+      <div className="bg-white rounded-xl p-4 max-w-xs mx-auto flex items-center justify-between border border-black">
+        <div>
+          <p className="text-black text-sm font-light">Trusted Freelancers</p>
+        </div>
+        <div className="flex space-x-1">
+          <span className="text-yellow-500 text-sm">⭐️⭐️⭐️⭐️⭐️</span>
+        </div>
+        <div className="flex -space-x-3">
+          <div className="w-6 h-6 bg-pink-300 rounded-full border-2 border-white" />
+          <div className="w-6 h-6 bg-blue-300 rounded-full border-2 border-white" />
+          <div className="w-6 h-6 bg-yellow-300 rounded-full border-2 border-white" />
+          <div className="w-6 h-6 bg-purple-300 rounded-full border-2 border-white" />
+        </div>
+      </div>
     </div>
   );
 }
