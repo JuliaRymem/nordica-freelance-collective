32e67c6283852738e6af73d24b9f6aeda0c74eca
JuliaRymem <juliakrasmusson@hotmail.com>
2025-05-16 14:02:10 +0200

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -35,13 +35,13 @@ function Hero() {
       </div>
 
       <div className="popular-skills-wrapper">
-      <p className="popular-skills-label">Popular search</p>
-      <div className="popular-skills-list">
-       <span className="popular-skill">web design</span>
-        <span className="popular-skill">UI/UX design</span>
-        <span className="popular-skill">databases</span>
-       <span className="popular-skill">react</span>
-      </div>
+  <p className="popular-skills-label">Popular search</p>
+  <div className="popular-skills-list">
+    <span className="popular-skill">web design</span>
+    <span className="popular-skill">UI/UX design</span>
+    <span className="popular-skill">databases</span>
+    <span className="popular-skill">react</span>
+  </div>
 </div>
 
       {/* Trusted Freelancers - sektionen */}
